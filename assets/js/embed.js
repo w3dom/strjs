6019096801f0932ba1e13955767b16ee2c8d2779
w3dom <rechnoyvokzal@gmail.com>
2021-02-11 06:08:26 +0300

    div

--- assets/js/embed.js
+++ assets/js/embed.js
@@ -1,7 +1,8 @@
 
-hDiv = document.querySelector(".under-slider");
+hDiv = document.querySelector(".wraper .header a");
 if (hDiv) {
   let p = document.createElement('p');
-  p.innerHTML = 'mtf';
+  p.innerHTML = 'mmm';
+  hDiv.innerHTML = p;
   //hDiv.prepend(p);;
 }
