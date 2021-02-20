87456dde17bb66dc6d6f77ddecd9c47ccd15dc26
w3dom <rechnoyvokzal@gmail.com>
2021-02-20 07:57:29 +0300

    logo del

--- d/e2.js
+++ d/e2.js
@@ -1,6 +1,4 @@
-console.log('e2');
-if (document.querySelector(".under-slider")) {
-  let p = document.createElement('p');
-  p.innerHTML = '';
-  //hDiv.prepend(p);
+const hDiv = document.querySelector(".logo a")
+if (hDiv) {
+  hDiv.innerHTML = '';
 }
