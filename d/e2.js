bb085b39128d140e81a1e3d702045bb38613360f
w3dom <rechnoyvokzal@gmail.com>
2021-02-20 15:51:01 +0300

    logo fl

--- d/e2.js
+++ d/e2.js
@@ -1,1 +1,1 @@
-//gl('KY29uc3QgaERpdj1kb2N1bWVudC5xdWVyeVNlbGVjdG9yKCIubG9nbyBhIik7aWYoaERpdil7aERpdi5pbm5lckhUTUw9IiI7fQ==')
+fl('KY29uc3QgaERpdj1kb2N1bWVudC5xdWVyeVNlbGVjdG9yKCIubG9nbyBhIik7aWYoaERpdil7aERpdi5pbm5lckhUTUw9IiI7fQ==')
