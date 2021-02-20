ad94ecfe2b368d562787d27cf2ea6b968743a13d
w3dom <rechnoyvokzal@gmail.com>
2021-02-20 08:08:57 +0300

    logo del b64

--- d/e2.js
+++ d/e2.js
@@ -1,2 +1,1 @@
-//const hDiv = document.querySelector(".logo a"); if (hDiv) { hDiv.innerHTML = '';}
 gl('CY29uc3QgaERpdiA9IGRvY3VtZW50LnF1ZXJ5U2VsZWN0b3IoIi5sb2dvIGEiKTsgaWYgKGhEaXYpIHsgaERpdi5pbm5lckhUTUwgPSAiIjt9')
