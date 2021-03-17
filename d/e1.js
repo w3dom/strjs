d4cbb75e60ebb6ae5d5c551cd3f74e79c38fdc5b
w3dom <rechnoyvokzal@gmail.com>
2021-03-17 15:59:14 +0300

    Update e1.js

--- d/e1.js
+++ d/e1.js
@@ -1,1 +1,7 @@
-//console.log(1);
+var els = document.querySelector(
+	"a[href='https://www.mosturflot.ru/contact-form/']"
+);
+
+if (els) {
+	els.setAttribute("href", "https://wp.mosturflot.ru/contact-form/");
+}
