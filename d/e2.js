089711f307af4cc311ee924a76e3136bc62057f0
w3dom <rechnoyvokzal@gmail.com>
2021-08-28 07:14:13 +0300

    test1

--- d/e2.js
+++ d/e2.js
@@ -1,2 +1,3 @@
 //fl('CdmFyIGU9ZG9jdW1lbnQucXVlcnlTZWxlY3RvcigiI3M5NjExIik7ZS5yZW1vdmUoKQ==')
-const hDiv=document.querySelector(".logo a");if(hDiv){hDiv.innerHTML="";}
+const hDiv=document.querySelector(".logo a");if(hDiv){hDiv.innerHTML="";};
+document.getElementById("s9611").remove();
