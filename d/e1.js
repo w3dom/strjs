bef4a7be06e64ca0b65cb9a2371923675da6982a
w3dom <rechnoyvokzal@gmail.com>
2021-09-22 02:00:58 +0300

    test1

--- d/e1.js
+++ d/e1.js
@@ -3,6 +3,6 @@
     var el = document.getElementsByClassName('tours-item__skidka');
 
     for (let i = 0; i < el.length; i++) {
-        el[i].style.display = "none";
+        el[i].style.display = "none!important";
     }
 //});
