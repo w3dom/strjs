0e0d2734bb8d72cb56bddc91114e63a669dc87f4
w3dom <rechnoyvokzal@gmail.com>
2021-09-22 02:48:48 +0300

    test1

--- d/e1.js
+++ d/e1.js
@@ -1,9 +1,8 @@
 
 //document.addEventListener('DOMContentLoaded', function() {
-    //var el = document.getElementById('featured-skidki').remove();
+    var el = document.getElementById('featured-skidki').remove();
 
     // for (let i = 0; i < el.length; i++) {
     //     el[i].style.display = "none";
     // }
 //});
-fl('dmFyIGVsID0gZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoImZlYXR1cmVkLXNraWRraSIpLnJlbW92ZSgpO2RvY3VtZW50LmdldEVsZW1lbnRCeUlkKCJzOTYxMSIpLnJlbW92ZSgpOw==');
