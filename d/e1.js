86ffdd5aa10559353c67de558c74bcd1507073f0
w3dom <rechnoyvokzal@gmail.com>
2021-09-22 02:14:13 +0300

    test1

--- d/e1.js
+++ d/e1.js
@@ -1,5 +1,5 @@
 
-document.addEventListener('DOMContentLoaded', function() {
+//document.addEventListener('DOMContentLoaded', function() {
     var el = document.getElementById('featured-skidki').remove();
 
-});
+//});
