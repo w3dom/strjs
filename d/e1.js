fed838a407028c54ef942db960b9baac6127797d
w3dom <rechnoyvokzal@gmail.com>
2021-09-22 01:57:32 +0300

    test1

--- d/e1.js
+++ d/e1.js
@@ -3,6 +3,6 @@ document.addEventListener('DOMContentLoaded', function() {
     var el = document.getElementsByClassName('tours-item__skidka');
 
     for (let i = 0; i < el.length; i++) {
-        el[i].remove();
+        el[i].style.color = "red";
     }
 });
