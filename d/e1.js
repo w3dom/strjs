9796b5f9c263fba3030f507181f0aece5095f1b5
w3dom <rechnoyvokzal@gmail.com>
2021-09-24 07:18:41 +0300

    test2

--- d/e1.js
+++
@@ -1,6 +0,0 @@
-if(document.getElementById('featured-skidki')|| document.getElementById('tours')) {
-    document.getElementById('featured-skidki').remove();
-    if(document.getElementById('tours')){
-        document.getElementById('tours').remove();
-    };
-};
